e54fde04895eadea41a50237e3e41c2506dd3447
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-11-19 09:51:08 +0800

    Update FeedbackData.cs

--- CSharpTcpDemo/com.dobot.api/FeedbackData.cs
+++ CSharpTcpDemo/com.dobot.api/FeedbackData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -97,7 +97,9 @@ namespace CSharpTcpDemo.com.dobot.api
         public byte JawButtonSignal = 0; //按钮板夹爪控制信号
         public byte SixForceOnline = 0;//六维力在线状态
 
-        public byte[] Reserved6 = new byte[82];//保留位
+        public byte[] Reserve6 = new byte[66]; // 保留位2
+        public double VibrationDisZ = 0; // 加速度计测量Z轴抖动位移
+        public ulong CurrentCommandID = 0; // 当前命令ID
 
         public double[] MActual = new double[6];//实际扭矩
         public double Load = 0;//负载重量kg
